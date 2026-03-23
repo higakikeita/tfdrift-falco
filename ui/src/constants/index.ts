95e7b78716b6c8b8095b9d57a63e53a2d76caeae
Keita Higaki <higaki.keichan@gmail.com>
2026-03-24 01:32:41 +0900

    fix(ui): add missing exports to constants barrel (SEVERITY_ICONS, CHANGE_TYPE_CONFIG, etc)

--- ui/src/constants/index.ts
+++ ui/src/constants/index.ts
@@ -7,12 +7,16 @@ export {
   SEVERITY_DOT_COLORS,
   SEVERITY_CONFIG,
   SEVERITY_ORDER,
+  SEVERITY_ICONS,
   PROVIDER_CONFIG,
+  PROVIDER_FILTER_COLORS,
   TIME_INTERVALS,
+  CHANGE_TYPE_CONFIG,
 } from './severity';
 
 export type {
   SeverityLevel,
   SeverityConfig,
   ProviderKey,
+  ChangeType,
 } from './severity';
